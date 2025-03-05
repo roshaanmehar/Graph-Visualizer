0da2fd36e553399edeac366664d3739d13e05aa3
Roshaan Ali <roshaanalimeher@gmail.com>
2025-03-05 15:22:50 +0000

    General Features Enhancement

---
+++ app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VoltageCurrentGraph from './VoltageCurrentGraph';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Voltage vs Current Graph</h1>
+      <VoltageCurrentGraph />
+    </div>
+  );
+}
+
+export default App;
